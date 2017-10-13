45962a812dba53c409f94988cf8ab04af46ea682
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2017-10-13 16:44:36 +0300

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const app = require('express')()
+const mongo = require('mongodb').MongoClient
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+const candidate = ''
+const url = `mongodb://localhost:27017/${candidate}`
+
+mongo.connect(url, (err, db) => {
+  console.log('connected to DB.')
+
+  app.get('/', function(req, res) {
+    res.send('Hello World!')
+  })
+
+  app.listen(3001, function() {
+    console.log('Example app listening on port 3001!')
+  })
 })
 
-app.listen(3001, function() {
-  console.log('Example app listening on port 3001!')
-})
